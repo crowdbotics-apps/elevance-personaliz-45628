d1f375ea0d4976fa4565df5e5422b4a216785ff3
Crowdbotics <team@crowdbotics.com>
2024-01-26 22:46:06 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "elevance-personaliz-45628",
+  url: "https://elevance-personaliz-45628.botics.co",
+  api: "https://elevance-personaliz-45628.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
